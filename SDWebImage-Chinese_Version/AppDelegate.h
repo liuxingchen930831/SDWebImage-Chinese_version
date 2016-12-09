689d4b2f5f327403452c859971cad732aa21c63d
liuxingchen <liuxingchen0831@gmail.com>
2016-12-09 16:09:59 +0800

    SDWebImage中文注解版

---
+++ SDWebImage-Chinese_Version/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SDWebImage-Chinese_Version
+//
+//  Created by liuxingchen on 16/12/9.
+//  Copyright © 2016年 Liuxingchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SDWebImage-Chinese_Version/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDWebImage-Chinese_Version
+//
+//  Created by liuxingchen on 16/12/9.
+//  Copyright © 2016年 Liuxingchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
